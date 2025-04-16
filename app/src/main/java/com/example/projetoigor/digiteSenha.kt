e0b549c7d8fa1909281e23463fbe8713654a5e2f
Leticia Furuse <leticiafurusi2005@gmail.com>
2025-04-16 03:46:17 -0300

    finalizando conflito das telas principais

--- app/src/main/java/com/example/projetoigor/digiteSenha.kt
+++ app/src/main/java/com/example/projetoigor/digiteSenha.kt
@@ -2,17 +2,45 @@ package com.example.projetoigor
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class digiteSenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_digite_senha)
+        val edit1 = findViewById<EditText>(R.id.editText1)
+        val edit2 = findViewById<EditText>(R.id.editText2)
+        val edit3 = findViewById<EditText>(R.id.editText3)
+        val edit4 = findViewById<EditText>(R.id.editText4)
+
+        // Função para avançar o foco
+        fun autoMove(current: EditText, next: EditText?) {
+            current.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    if (s?.length == 1) {
+                        next?.requestFocus()
+                    }
+                }
+            })
+        }
+
+        // Aplica a lógica nos campos
+        autoMove(edit1, edit2)
+        autoMove(edit2, edit3)
+        autoMove(edit3, edit4)
+
+
+
+
+        //logica do botao para outra tela
         val btnProximaTela : Button = findViewById(R.id.btnConfirmarSenha)
         btnProximaTela.setOnClickListener{
             val intent = Intent(this,transferenciaRealizada::class.java)
